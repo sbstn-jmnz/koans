be21644112fe81755b58d44ab85249baa5cd9ea0
sbstn <sbstn.jmnz@yahoo.com>
2015-07-12 23:56:30 -0300

    about_modules

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+ class DiceSet
+  attr_reader :values
+
+  def roll(num) 
+  @values = []
+  (1..num).each do 
+    @values << rand(1..6)
+    end  
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -14,7 +21,6 @@ class AboutDiceProject < Neo::Koan
 
   def test_rolling_the_dice_returns_a_set_of_integers_between_1_and_6
     dice = DiceSet.new
-
     dice.roll(5)
     assert dice.values.is_a?(Array), "should be an array"
     assert_equal 5, dice.values.size
